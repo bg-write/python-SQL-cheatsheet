f8d057ed9d1cc60ecce3f0b03045e043448d2dec
bg-write <bradywgerber@gmail.com>
2023-03-16 10:03:02 -0400

    Add reverse string problem

--- py_guides/10_coding_challenges.py
+++ py_guides/10_coding_challenges.py
@@ -1,5 +1,7 @@
+# ---------------------------------------------------
 # Palindrome
 # Check if a string is spelled the same both forward and backward, ignoring case and non-alphanumeric characters (punctuation, spaces, symbols)
+
 import re
 
 str_to_check = 'raceC@_ar'
@@ -7,18 +9,17 @@ str_to_check = 'raceC@_ar'
 
 def palindrome(str):
     str_alphabet = re.sub(r'[\W_]', '', str)
-    # [^...] / if we catch a character NOT in the ranges stated after ^, then replace it with ''
-    # https://regex101.com/
     str_clean = str_alphabet.lower().replace(' ', '')
     return str_clean == str_clean[::-1]
-    # [::-1] is slice notation for "start at the end, count down to the beginning, and take one step back at a time"
 
 
 palindrome(str_to_check)  # True
 
+# ---------------------------------------------------
 # FizzBuzz
 # Write a program that prints any range of numbers. For multiples of three, print "Fizz" instead of the number. For multiples of five, print "Buzz." For numbers that are multiples of both three and five, print "FizzBuzz."
-for num in range(1, 31):
+
+for num in range(1, 16):
     str = ''
     if num % 3 == 0:
         str = str + 'Fizz'
@@ -28,9 +29,11 @@ for num in range(1, 31):
         str = num
     print(str)
 
-
+# ---------------------------------------------------
 # Fibonacci
 # Using recursion, find the nth element within the Fibonacci Sequence
+
+
 def fibonacci(n):
     if n < 2:
         return n
@@ -46,11 +49,33 @@ fibonacci(4)  # 3
 fibonacci(5)  # 5
 fibonacci(6)  # 8
 
+# ---------------------------------------------------
 # Euclidian Distance
 # Find an Euclidian distance between (2, 3) and (10, 8) aka find the shortest distance between two or more points in two or more dimensions
+
 from math import dist
 
 point_1 = (2, 3)
 point_2 = (10, 8)
 euclidian_distance = dist(point_1, point_2)
 # 9.433981132056603
+
+# ---------------------------------------------------
+# Reverse String
+# Write a function that reverses a string.
+# The input string is given as an array of characters s.
+# You must do this by modifying the input array in-place with O(1) extra memory.
+
+s1 = ['h', 'e', 'l', 'l', 'o']
+s2 = ['H', 'a', 'n', 'n', 'a', 'h']
+
+
+def reverse_string(s):
+    return s[::-1]
+
+
+print(reverse_string(s1))  # ['o', 'l', 'l', 'e', 'h']
+print(reverse_string(s2))  # ['h', 'a', 'n', 'n', 'a', 'H']
+
+# ---------------------------------------------------
+# TBD
